8028db9a1fce458480a1c81e1a54ada726afc93f
Jack Morton <jmort87@gmail.com>
2024-08-16 19:13:27 -0400

    Change skill icon on client startup

---
+++ src/main/java/com/perfectbalance/PerfectBalanceConfig.java
@@ -0,0 +1,18 @@
+package com.perfectbalance;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("example")
+public interface PerfectBalanceConfig extends Config
+{
+//	@ConfigItem(
+//		keyName = "greeting",
+//		name = "Welcome Greeting",
+//		description = "The message to show to the user when they login"
+//	)
+//	default String greeting()
+//	{
+//		return "Hello";
+//	}
+}
---
+++ src/main/java/com/perfectbalance/PerfectBalancePlugin.java
@@ -0,0 +1,90 @@
+package com.perfectbalance;
+
+import com.google.inject.Provides;
+import javax.inject.Inject;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.widgets.Widget;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.ImageUtil;
+
+import java.awt.image.BufferedImage;
+
+@Slf4j
+@PluginDescriptor(
+	name = "Perfect Balance",
+	description = "Changes the skill tab icon to be perfectly balanced.",
+	enabledByDefault = false
+)
+public class PerfectBalancePlugin extends Plugin
+{
+	@Inject
+	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
+
+	@Override
+	protected void startUp() throws Exception
+	{
+		overrideSkillIcon();
+	}
+
+	@Override
+	protected void shutDown() throws Exception
+	{
+		client.getWidgetSpriteCache().reset();
+		setSkillIcon("898");
+	}
+
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	{
+		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
+		{
+			overrideSkillIcon();
+		}
+	}
+
+	private SpritePixels getFileSpritePixels(String file)
+	{
+		try
+		{
+			System.out.println("Loading: " + file);
+			BufferedImage image = ImageUtil.loadImageResource(this.getClass(), file);
+			return ImageUtil.getImageSpritePixels(image, client);
+		}
+		catch (RuntimeException ex)
+		{
+			System.out.println("Unable to load image: " + ex);
+		}
+
+		return null;
+	}
+
+	private void overrideSkillIcon()
+	{
+		clientThread.invoke(() ->
+		{
+			if (client.getGameState().getState() <= GameState.LOGIN_SCREEN.getState())
+			{
+				return false;
+			}
+			setSkillIcon("interfaceicon");
+			System.out.println("Attempting to override");
+			return true;
+		});
+	}
+
+	private void setSkillIcon(String iconName)
+	{
+		String spriteFile = iconName + ".png";
+		SpritePixels spritePixels = getFileSpritePixels(spriteFile);
+		client.getSpriteOverrides().put(898, spritePixels);
+	}
+}
